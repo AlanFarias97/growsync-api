4afcbade7e6599f430da92f8dd030db66b92d265
alanfarias97 <alanfarias_97@hotmail.com>
2026-05-02 18:06:38 -0300

    initial commit

---
+++ src/main/java/com/growsync/GrowsyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.growsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowsyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowsyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growsync/GrowsyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowsyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
